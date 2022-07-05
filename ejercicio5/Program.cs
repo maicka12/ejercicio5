52b3252cd7124da8ef500ac1b66d206c9f3940d6
maicka <maicka1997@gmail.com>
2022-07-05 04:08:32 +0200

    Agregar archivos de proyecto.

---
+++ ejercicio5/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp1
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+
+            int A = 21;
+            int B = 32;
+            int C = 12;
+            int D = 41;
+
+            B = C;
+            C = A;
+            A = D;
+            D = B;
+
+
+            Console.WriteLine(A);
+            Console.WriteLine(B);
+            Console.WriteLine(C);
+            Console.WriteLine(D);
+            Console.ReadKey();
+
+        }
+    }
+}
